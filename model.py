1a95e2ea54bcecac8e859703053d4b5d355e7aff
neemiasbsilva <neemiasbsilva@gmail.com>
2020-03-02 16:16:04 -0400

    Setting the Conv3d to Conv3D

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import keras
 from keras import layers
 from keras.model import Sequential, Model
-from keras.layers import Dense, Flatten, Conv3d
+from keras.layers import Dense, Flatten, Conv3D
 from keras.regularizers import l2
 import numpy as np
 
