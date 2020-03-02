df9eb4c8bcefaa80999510b52bd0b821a0351469
neemiasbsilva <neemiasbsilva@gmail.com>
2020-03-02 16:13:11 -0400

    Define the fully-connected layer (Dense)

--- model.py
+++ model.py
@@ -5,3 +5,16 @@ from keras.layers import Dense, Flatten, Conv3d
 from keras.regularizers import l2
 import numpy as np
 
+
+def fully_connected(x, nf, name, weight_decay):
+
+    kerner_reg = l2(weight_decay[0]) if weight_decay else None
+    bias_reg = l2(weight_decay[1]) if weight_decay else None
+
+    x = Dense(nf, name=name, 
+                kernel_regularizer=kerner_reg,
+                bias_regulaizer=bias_reg,
+                kernel_initializer=random_normal(stddev=0.01),
+                bias_initializer=constant(0.0))(x)
+
+    return x
