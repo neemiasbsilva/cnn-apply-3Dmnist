96e0245d92c1b7af8989cfc2a309ffb4de78e91b
neemiasbsilva <neemiasbsilva@gmail.com>
2020-03-02 16:10:51 -0400

    Import the dependencies first

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import keras
 from keras import layers
-from keras.model import Sequential
+from keras.model import Sequential, Model
 from keras.layers import Dense, Flatten, Conv3d
+from keras.regularizers import l2
 import numpy as np
+
