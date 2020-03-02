930b77043659c79699682d696ed71134d1e54e0a
neemiasbsilva <neemiasbsilva@gmail.com>
2020-03-02 16:47:26 -0400

    Preparation the data and Adding helper function

--- preparation_data.py
+++ preparation_data.py
@@ -25,24 +25,3 @@ def get_dataset(path):
         # Reshape data into 3D format
         x_train = rgb_data_trainsfor(x_train)
 
-
-# -- Process code --
-# Load the HDF5 data file
-with h5py.File("./full_dataset_vectors.h5", "r") as hf:
-
-    # Split the data into training/test features/targets
-    X_train = hf["X_train"][:]
-    targets_train = hf["y_train"][:]
-    X_test = hf["X_test"][:]
-    targets_test = hf["y_test"][:]
-
-    # Determine sample shape
-    sample_shape = (16, 16, 16, 3)
-
-    # Reshape data into 3D format
-    X_train = rgb_data_transform(X_train)
-    X_test = rgb_data_transform(X_test)
-
-    # Convert target vectors to categorical targets
-    targets_train = to_categorical(targets_train).astype(np.integer)
-    targets_test = to_categorical(targets_test).astype(np.integer)
