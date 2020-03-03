9941d121142a568b973ce17d452ef11d74e8b03c
neemiasbsilva <neemiasbsilva@gmail.com>
2020-03-03 13:40:45 -0300

    Define the command line arguments

--- train.py
+++ train.py
@@ -4,3 +4,21 @@ from keras.optimizers import SGD
 import argparse
 
 
+parser = argparse.ArgumentParser(description='Training Three Dimension CNN')
+
+parser.add_argument("-batch_size", action="store", required=True, help="batch size", type=int)
+parser.add_argument("-no_epochs", action="store", required=True, help="Number of epochs", type=int)
+parser.add_argument("-learning_rate", action="store", default=0.001, required=False, help="Learning Rate", type=float)
+parser.add_argument("-validation_split", action="store", default=0.2, required=False, help="Validation Split", type=float)
+parser.add_argument("-verbosity", action="store", default=1, required=False, help="Verbosity", type=float)
+parser.add_argument("-path_data", action="store", required=True, help="The dataset of the 3Dmnist path", dest='experiment_name')
+
+
+arguments = parser.parse_args()
+
+batch_size = arguments.batch_size
+no_epochs = arguments.no_epochs
+lr = arguments.learning_rate
+val_split = arguments.validation_split
+verbosity = arguments.verbosity
+path_data = arguments.path_data
