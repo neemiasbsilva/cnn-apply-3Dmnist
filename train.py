0ee076f42c91175c00a683ec52ee248ec2b2f416
neemiasbsilva <neemiasbsilva@gmail.com>
2020-03-03 14:26:09 -0300

    Compile the model and fit the data to model

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ parser = argparse.ArgumentParser(description='Training Three Dimension CNN')
 parser.add_argument("-batch_size", action="store", required=True, help="batch size", type=int)
 parser.add_argument("-no_epochs", action="store", required=True, help="Number of epochs", type=int)
 parser.add_argument("-learning_rate", action="store", default=0.001, required=False, help="Learning Rate", type=float)
+parser.add_argument('-momentum', action="store", default=0.9, required=False, help="Momentum term", type=float)
 parser.add_argument("-validation_split", action="store", default=0.2, required=False, help="Validation Split", type=float)
 parser.add_argument("-verbosity", action="store", default=1, required=False, help="Verbosity", type=float)
 parser.add_argument("-path_data", action="store", required=True, help="The dataset of the 3Dmnist path", dest='experiment_name')
@@ -19,6 +20,7 @@ arguments = parser.parse_args()
 batch_size = arguments.batch_size
 no_epochs = arguments.no_epochs
 lr = arguments.learning_rate
+momentum = arguments.momentum
 val_split = arguments.validation_split
 verbosity = arguments.verbosity
 path_data = arguments.path_data
@@ -27,3 +29,13 @@ path_data = arguments.path_data
 x_train, y_train, x_test, y_test = get_dataset(path_data)
 
 model = get_training_model(sample_shape=(16, 16, 16, 3))
+
+opt = SGD(lr=lr, momentum=momentum)
+
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+history = model.fit(x_train, y_train, 
+            batch_size=batch_size,
+            epochs=no_epochs,
+            verbose=verbosity,
+            validation_split=val_split)
