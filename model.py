6805a1fcad7a46a9fcd178166fa1fa499b8231d3
neemiasbsilva <neemiasbsilva@gmail.com>
2020-03-05 20:26:44 -0400

    Using dropout in hidden layers

--- model.py
+++ model.py
@@ -64,7 +64,7 @@ def get_training_model(sample_shape, dimension=3, layer_name='block1_conv3d', we
     
     model.add(MaxPooling3D(pool_size=(2, 2, 2)))
 
-    model.add(Conv3D(256, (3, 3, 3), activation='relu'))
+    model.add(Conv3D(128, (3, 3, 3), activation='relu'))
 
     model.add(MaxPooling3D(pool_size=(2, 2, 2)))
 
